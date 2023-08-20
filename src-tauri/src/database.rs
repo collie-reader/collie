d6b76692eaa3a247fd297ab88bd1197b89792bca
parksb <parkgds@gmail.com>
2023-08-20 22:06:33 +0900

    Set up sqlite database

---
+++ src-tauri/src/database.rs
@@ -0,0 +1,80 @@
+use rusqlite::{Connection, Result};
+use sea_query::{ColumnDef, Expr, ForeignKey, ForeignKeyAction, Iden, SqliteQueryBuilder, Table};
+
+#[derive(Iden)]
+pub enum Feeds {
+    Table,
+    Id,
+    Title,
+    Link,
+    CheckedAt,
+}
+
+#[derive(Iden)]
+pub enum Items {
+    Table,
+    Id,
+    Author,
+    Title,
+    Description,
+    Link,
+    Status,
+    PublishedAt,
+    Feed,
+}
+
+pub fn init() -> Result<()> {
+    let conn = Connection::open("collie.db")?;
+
+    let create_table_feeds = Table::create()
+        .table(Feeds::Table)
+        .if_not_exists()
+        .col(
+            ColumnDef::new(Feeds::Id)
+                .integer()
+                .not_null()
+                .auto_increment()
+                .primary_key(),
+        )
+        .col(ColumnDef::new(Feeds::Title).text().not_null())
+        .col(ColumnDef::new(Feeds::Link).text().not_null())
+        .col(ColumnDef::new(Feeds::CheckedAt).date_time().not_null())
+        .build(SqliteQueryBuilder);
+
+    let create_table_items = Table::create()
+        .table(Items::Table)
+        .if_not_exists()
+        .col(
+            ColumnDef::new(Items::Id)
+                .integer()
+                .not_null()
+                .auto_increment()
+                .primary_key(),
+        )
+        .col(ColumnDef::new(Items::Author).text())
+        .col(ColumnDef::new(Items::Title).text().not_null())
+        .col(ColumnDef::new(Items::Description).text().not_null())
+        .col(ColumnDef::new(Items::Link).text().not_null())
+        .col(
+            ColumnDef::new(Items::Status)
+                .text()
+                .check(Expr::col(Items::Status).is_in(["unread", "read", "saved"]))
+                .not_null()
+                .default("unread"),
+        )
+        .col(ColumnDef::new(Items::PublishedAt).date_time().not_null())
+        .col(ColumnDef::new(Items::Feed).text().not_null())
+        .foreign_key(
+            ForeignKey::create()
+                .name("fk_items_feeds")
+                .from(Items::Table, Items::Feed)
+                .to(Feeds::Table, Feeds::Id)
+                .on_delete(ForeignKeyAction::Cascade)
+                .on_update(ForeignKeyAction::Cascade),
+        )
+        .build(SqliteQueryBuilder);
+
+    conn.execute_batch(&[create_table_feeds, create_table_items].join(";"))?;
+
+    Ok(())
+}
