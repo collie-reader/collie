7a6c1132c106fa62cfe03c4333b7009933c55f8f
parksb <parkgds@gmail.com>
2023-08-22 12:12:55 +0900

    Improve styles of items page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {A, Route, Routes} from "@solidjs/router";
 
 import "./App.css";
 import { ItemStatus } from "./api/items";
+import {ItemType} from "./routes/Items";
 
 const Items = lazy(() => import("./routes/Items"));
 const Feeds = lazy(() => import("./routes/Feeds"));
@@ -15,16 +16,16 @@ function App() {
     <div class="container">
       <div class="navigation row">
         <h1>Collie</h1>
-        <A href="/">Unread</A>
+        <A href="/">Inbox</A>
+        <A href="/unread">Unread</A>
         <A href="/saved">Saved</A>
-        <A href="/read">Read</A>
         <A href="/feeds">Feeds</A>
         <A href="/settings">Settings</A>
       </div>
       <Routes>
-        <Route path="/" element={<Items status={ItemStatus.UNREAD} />} />
-        <Route path="/saved" element={<Items status={ItemStatus.SAVED} />} />
-        <Route path="/read" element={<Items status={ItemStatus.READ} />} />
+        <Route path="/" element={<Items type={ItemType.INBOX} />} />
+        <Route path="/unread" element={<Items type={ItemType.UNREAD} />} />
+        <Route path="/saved" element={<Items type={ItemType.SAVED} />} />
         <Route path="/feeds" component={Feeds} />
         <Route path="/settings" component={Settings} />
       </Routes>
--- src/routes/Items.tsx
+++ src/routes/Items.tsx
@@ -1,31 +1,66 @@
 import { createSignal, For, Match, onMount, Show, Switch } from "solid-js";
 
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
+
+import "../styles/Items.css";
 import * as api from "../api/items";
 
+export enum ItemType {
+  INBOX = "Inbox",
+  UNREAD = "Unread",
+  SAVED = "Saved",
+}
+
+function typeToStatus(type: ItemType) {
+  switch (type) {
+    case ItemType.INBOX:
+      return null;
+    case ItemType.UNREAD:
+      return api.ItemStatus.UNREAD;
+    case ItemType.SAVED:
+      return api.ItemStatus.SAVED;
+  }
+}
+
 interface Props {
-  status: api.ItemStatus;
+  type: ItemType;
 }
 
 function Items(props: Props) {
+  const LIMIT = 50;
+  const givenStatus = typeToStatus(props.type);
+
   const [offset, setOffset] = createSignal(0);
-  const [opt, setOpt] = createSignal<api.ItemReadOption>({ status: props.status, offset: offset(), limit: 50 });
+  const [opt, setOpt] = createSignal<api.ItemReadOption>({ status: givenStatus, offset: offset(), limit: LIMIT });
 
   const [items, setItems] = createSignal<api.Item[]>([]);
   const [selectedItem, setSelectedItem] = createSignal<api.Item | null>(null);
 
   const loadNext = async () => {
     setOffset(offset() + 1);
+    window.scroll(0, 0);
+    setItems(await api.readItems({ ...opt(), offset: offset() }));
+  };
+
+  const loadFirst = async () => {
+    setOffset(0);
+    window.scroll(0, 0);
     setItems(await api.readItems({ ...opt(), offset: offset() }));
   };
 
   const loadPrev = async () => {
     setOffset(offset() - 1);
+    window.scroll(0, 0);
     setItems(await api.readItems({ ...opt(), offset: offset() }));
   };
 
   const markAs = async (id: number, status: api.ItemStatus) => {
-    await api.markAs(id, status);
-    setItems(await api.readItems(opt()));
+    if (status !== givenStatus) {
+      await api.markAs(id, status);
+      setItems(await api.readItems(opt()));
+    }
   };
 
   onMount(async () => {
@@ -36,32 +71,38 @@ function Items(props: Props) {
     <div class="container">
       <div class="row">
         <div class="item-list">
-          <h2>{props.status.valueOf()}</h2>
-          <div>
-            <ul>
-              <For each={items()}>{(item: api.Item) =>
-                <li>
-                  <a href={item.link} target="_blank">{item.title}</a>
-                  <span>by {item.author}</span>
-                  <span>on {item.feed}</span>
-                  <span>at {item.published_at}</span>
-                  <a onClick={() => markAs(item.id, api.ItemStatus.SAVED)}>Save</a>
-                  <a onClick={() => setSelectedItem(item)}>More</a>
-                </li>
-              }</For>
-            </ul>
-          </div>
+          <h2>{props.type.valueOf()}</h2>
+          <ul>
+            <For each={items()}>{(item: api.Item) =>
+              <li class={`${item.status == api.ItemStatus.READ ? "lowp" : ""}`}>
+                <strong><a href={item.link} target="_blank"
+                  onClick={() => markAs(item.id, api.ItemStatus.READ)}>{item.title}</a></strong>
+                <small class="row">
+                  <span class="sep">by</span> {item.author} <span title={item.published_at}>{dayjs(item.published_at).fromNow()}</span>
+                  <span class="sep"> | </span><a onClick={() => markAs(item.id, api.ItemStatus.SAVED)}>Save</a>
+                  <span class="sep"> | </span><a onClick={() => {
+                    setSelectedItem(item);
+                    markAs(item.id, api.ItemStatus.READ)
+                  }}>More</a>
+                </small>
+              </li>
+            }</For>
+          </ul>
           <div class="row">
-            <Show when={offset() > 0}>
-              <button onClick={() => loadPrev()}>←</button>
+            <Show when={offset() > 3}>
+              <button onClick={() => loadFirst()}>←← 1</button>
             </Show>
-            <span>{offset()}</span>
-            <button onClick={() => loadNext()}>→</button>
+            <Show when={offset() > 0}>
+              <button onClick={() => loadPrev()}>← {offset()}</button>
+            </Show>
+            <Show when={items().length >= LIMIT}>
+              <button onClick={() => loadNext()}>{offset() + 2} →</button>
+            </Show>
           </div>
         </div>
         <Show when={selectedItem()}>
-          <div class="item-viewer">
-            <div>
+          <div class="item-viewer-container">
+            <div class="item-viewer">
               <h3>{selectedItem()?.title}</h3>
               <div innerHTML={selectedItem()?.description} />
             </div>
