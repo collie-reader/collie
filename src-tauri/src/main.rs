4fcc13cb11a7d516611e7af357b7076320ffdbb5
parksb <parkgds@gmail.com>
2023-08-25 09:14:25 +0900

    Branch app data directory according to build environment

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use rusqlite::Connection;
-use std::{fs, sync::Mutex};
+use std::{fs, path::PathBuf, sync::Mutex};
 use tauri::Manager;
 
 pub mod models {
@@ -43,7 +43,12 @@ fn main() {
             commands::settings::update_setting,
         ])
         .setup(|app| {
-            let app_data_dir = app.handle().path_resolver().app_data_dir().unwrap();
+            let app_data_dir = if cfg!(dev) {
+                PathBuf::from("data")
+            } else {
+                app.handle().path_resolver().app_data_dir().unwrap()
+            };
+
             fs::create_dir_all(&app_data_dir).unwrap();
             let db = models::database::open_connection(&app_data_dir).unwrap();
             let _ = models::database::migrate(&db);
